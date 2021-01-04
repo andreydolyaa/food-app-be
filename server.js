82f66b039be4a71a17f2f1601142425b7fdbb010
Andrey Dolya <dolya7kk@gmail.com>
2021-01-04 17:54:27 +0200

    Mail service added

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
-const port = 3000;
+const port = 3001;
+const orderService = require('./services/order.service');
 
 app.use(bodyParser.json());
 
@@ -13,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:8081','http://127.0.0.1:3001', 'http://localhost:3001'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000', 'http://localhost:8081', 'http://127.0.0.1:3001', 'http://localhost:3001'],
         credentials: true
     };
     app.use(cors(corsOptions));
@@ -26,6 +27,12 @@ const restaurantsRoutes = require('./api/restaurant/restaurant.routes');
 
 app.use('/api/restaurants', restaurantsRoutes);
 
+
+
+app.post('/submitOrder', (req, res) => {
+    orderService.sendEmail(req.body);
+    res.end();
+})
 
 
 http.listen(port, () => {
---
+++ services/order.service.js
@@ -0,0 +1,47 @@
+
+const nodemailer = require('nodemailer');
+
+
+function sendEmail(data) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'fooderplacecom@gmail.com',
+            pass: 'place12A'
+        }
+    });
+
+    var dishList = data.orderDetails.map(dish => {
+        return `
+        <tr>
+        <td style="border:1px solid black;padding:10px;margin-right:20px">${dish.name} $${dish.price}</td>
+        </tr>
+        `
+    }).join('');
+
+    var mailOptions = {
+        from: 'fooderPlaceCom@gmail.com',
+        to: data.userDetails.email,
+        subject: 'Your Order From FooderPlace.com',
+        html: `
+        <h2>Hi ${data.userDetails.firstName}, we are preparing your order!</h2>
+        ${dishList}
+        <p>The total charge is - $${data.totalPrice}</p>
+        <p>Thank you for using our service, hope to see you soon again!</p>
+        <p>fooderPlace.com</p>
+        `
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
+}
+
+module.exports = {
+    sendEmail
+}
