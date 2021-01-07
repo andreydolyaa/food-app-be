9233d6657429bb4b5548556a2faa92593e330143
Andrey Dolya <dolya7kk@gmail.com>
2021-01-08 01:24:30 +0200

    SDF

---
+++ config/conf.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+    user: 'fooderplacecom@gmail.com',
+    pass: 'place12A1991',
+    secret: 'ANDREYDOLYA',
+    // dbUrl:'mongodb://localhost:27017'
+    "dbURL": 'mongodb+srv://andrey91:andrey1991dolya@cluster0.eqd4y.mongodb.net/restDB?retryWrites=true&w=majority'
+}
