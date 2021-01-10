bf0bbe87800079c2d61a1d5bb8a7e35a3adccd6e
andreydolyaa <71754059+andreydolyaa@users.noreply.github.com>
2021-01-10 20:58:53 +0200

    Delete conf.js

--- config/conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    user: 'fooderplacecom@gmail.com',
-    pass: 'place12A1991',
-    secret: 'ANDREYDOLYA',
-    // dbUrl:'mongodb://localhost:27017'
-    "dbURL": 'mongodb+srv://andrey91:andrey1991dolya@cluster0.eqd4y.mongodb.net/restDB?retryWrites=true&w=majority'
-}
